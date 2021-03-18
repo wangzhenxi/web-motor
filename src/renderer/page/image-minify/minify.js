e0e56f5a4afb5adc11f61839703e21e258a2b178
wangzhenxi <im@joshwong.cn>
2021-03-18 19:58:32 +0800

    构建问题修复

--- src/renderer/page/image-minify/minify.js
+++ src/renderer/page/image-minify/minify.js
@@ -1,6 +1,6 @@
 
-import Https from "https";
-import Url from "url";
+import Https from 'https';
+import Url from 'url';
 import fs from 'fs';
 import chalk from 'chalk';
 import figures from 'figures';
@@ -10,49 +10,49 @@ const TINYIMG_URL = 'tinypng.com';
 
 function getRandomHeader() {
   // 随机IP
-  const ip = new Array(4).fill(0).map(() => parseInt(Math.random() * 255)).join(".");
+  const ip = new Array(4).fill(0).map(() => parseInt(Math.random() * 255)).join('.');
 
   return {
     headers: {
-        "Cache-Control": "no-cache",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Postman-Token": Date.now(),
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
-        "X-Forwarded-For": ip
+      'Cache-Control': 'no-cache',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Postman-Token': Date.now(),
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+      'X-Forwarded-For': ip,
     },
     hostname: TINYIMG_URL,
-    method: "POST",
-    path: "/web/shrink",
-    rejectUnauthorized: false
-  }
+    method: 'POST',
+    path: '/web/shrink',
+    rejectUnauthorized: false,
+  };
 }
 
 function uploadImg(file) {
   const opts = getRandomHeader();
   return new Promise((resolve, reject) => {
-      const req = Https.request(opts, res => {
-        res.on("data", data => {
-          const obj = JSON.parse(data.toString());
-          obj.error ? reject(obj.message) : resolve(obj);
-        })
+    const req = Https.request(opts, (res) => {
+      res.on('data', (data) => {
+        const obj = JSON.parse(data.toString());
+        obj.error ? reject(obj.message) : resolve(obj);
       });
-      req.write(file, "binary");
-      req.on("error", e => reject(e));
-      req.end();
+    });
+    req.write(file, 'binary');
+    req.on('error', e => reject(e));
+    req.end();
   });
 }
 
 function downloadImg(url) {
   const opts = new Url.URL(url);
   return new Promise((resolve, reject) => {
-      const req = Https.request(opts, res => {
-        let file = "";
-        res.setEncoding("binary");
-        res.on("data", chunk => file += chunk);
-        res.on("end", () => resolve(file));
-      });
-      req.on("error", e => reject(e));
-      req.end();
+    const req = Https.request(opts, (res) => {
+      let file = '';
+      res.setEncoding('binary');
+      res.on('data', chunk => file += chunk);
+      res.on('end', () => resolve(file));
+    });
+    req.on('error', e => reject(e));
+    req.end();
   });
 }
 
@@ -63,7 +63,7 @@ async function compressImg(filepath) {
     data: '',
   };
   try {
-    const file = fs.readFileSync(filepath, "binary");
+    const file = fs.readFileSync(filepath, 'binary');
     const obj = await uploadImg(file);
     const data = await downloadImg(obj.output.url);
     const oldSize = chalk.redBright(obj.input.size);
@@ -72,7 +72,7 @@ async function compressImg(filepath) {
     const msg = `${figures.tick} Compressed [${chalk.yellowBright(filepath)}] completed: Old Size ${oldSize}, New Size ${newSize}, Optimization Ratio ${ratio}`;
     Object.assign(res, {
       data,
-      msg
+      msg,
     });
   } catch (err) {
     const msg = `${figures.cross} Compressed [${chalk.yellowBright(filepath)}] failed: ${chalk.redBright(err)}`;
@@ -85,15 +85,13 @@ async function compressImg(filepath) {
 }
 
 async function minify(filepaths = []) {
-  const files = await Promise.all(
-    filepaths.map(async filepath => {
-      // 压缩
-      const res = await compressImg(filepath);
-      // 输出日志
-      console.log(res.msg);
-      return res;
-    })
-  )
+  const files = await Promise.all(filepaths.map(async (filepath) => {
+    // 压缩
+    const res = await compressImg(filepath);
+    // 输出日志
+    console.log(res.msg);
+    return res;
+  }));
 
   return files;
 }
